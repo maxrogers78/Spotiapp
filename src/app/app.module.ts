76580752a3a6e20a885d778e7d99e5f84fa060aa
Max Rogers <maxrogerssepulveda@gmail.com>
2020-07-26 20:55:22 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ArtistComponent,
+    SearchComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
